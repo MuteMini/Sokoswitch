fae0c2948c1f543263d0548f3d49e90c46cac0d1
MuteMini <63823931+MuteMini@users.noreply.github.com>
2020-06-16 13:23:50 -0400

    Initial commit

---
+++ sokoswitch/desktop/src/com/sokoswitch/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sokoswitch.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sokoswitch.SokoSwitch;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SokoSwitch(), config);
+	}
+}
