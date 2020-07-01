db46e9160d455ccb3c100a59468a46b421781126
MuteMini <63823931+MuteMini@users.noreply.github.com>
2020-07-01 18:21:45 -0400

    exported tiledset stuff

---
+++ sokoswitch/core/assets/levels/prototypeSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="prototypeSet" tilewidth="256" tileheight="256" tilecount="16" columns="4">
+ <image source="../puzzlePrototype2.png" width="256" height="256"/>
+</tileset>
