fe47a6d8e7d25392a76dc04696c675a73037e509
MuteMini <63823931+MuteMini@users.noreply.github.com>
2020-07-07 12:11:40 -0400

    changed up json parser

--- sokoswitch/core/src/sokoswitch/game/loaders/WorldData.java
+++ sokoswitch/core/src/sokoswitch/game/loaders/WorldData.java
@@ -9,17 +9,22 @@ import com.badlogic.gdx.math.Vector2;
 
 public class WorldData {
 
-	private ArrayList<Vector2> levelPos;
-	private String[] levelDisplay;
-	private int[] levelConnected;
-	private ArrayList<Long>[] levelPrereq;
+	public boolean horizontal;
+	public int[] levelSize;
+	public ArrayList<Vector2> levelPos;
+	public int[] levelConnected;
+	public String[] levelDisplay;
+	public ArrayList<Long>[] levelPrereq;
+	public int[] levelReqSize;
 	
 	@SuppressWarnings("unchecked")
 	public WorldData() {
+		this.levelSize = new int[2];
 		this.levelPos = new ArrayList<>();
 		this.levelConnected = new int[0];
 		this.levelDisplay = new String[0];
 		this.levelPrereq = new ArrayList[0];
+		this.levelReqSize = new int[0];
 	}
 
     public WorldData(FileHandle file) {
@@ -30,22 +35,6 @@ public class WorldData {
     public int getDataSize() {
     	return levelConnected.length;
     }
-    
-    public ArrayList<Vector2> getLevelPos() {
-    	return levelPos;
-    }
-	
-	public String getLevelDisplay(int index) {
-		return levelDisplay[index];
-	}
-	
-	public int getLevelConnected(int index) {
-		return levelConnected[index];
-	}
-	
-	public ArrayList<Long> getLevelPrereq(int index) {
-		return levelPrereq[index];
-	}
 	
 	@SuppressWarnings("unchecked")
 	private void setLevelData(FileHandle file) {
@@ -54,28 +43,44 @@ public class WorldData {
 			Reader reader = file.reader();
 			JSONObject jsonObject = (JSONObject) parser.parse(reader);
 			
+			Iterator<Long> sizeArr = ((JSONArray) jsonObject.get("size")).iterator();
+			levelSize[0] = Math.toIntExact(sizeArr.next());
+			levelSize[1] = Math.toIntExact(sizeArr.next());
+			
 			JSONArray posArr = (JSONArray) jsonObject.get("pos");
-			JSONArray displayArr = (JSONArray) jsonObject.get("displayText");
 			JSONArray connectArr = (JSONArray) jsonObject.get("connectedLevel");
+			JSONArray displayArr = (JSONArray) jsonObject.get("displayText");
 			JSONArray prereqArr = (JSONArray) jsonObject.get("prereq");
+			JSONArray reqSizeArr = (JSONArray) jsonObject.get("prereqSize");
 			
 			Iterator<JSONArray> posIterator = posArr.iterator();
-			Iterator<String> displayIterator = displayArr.iterator();
 			Iterator<Long> connectIterator = connectArr.iterator();
+			Iterator<String> displayIterator = displayArr.iterator();
 			Iterator<JSONArray> prereqIterator = prereqArr.iterator();
+			Iterator<Long> reqSizeIterator = reqSizeArr.iterator();
 			
+			this.horizontal = (boolean) jsonObject.get("horizontal");
+			this.levelPos = new ArrayList<>();
+			this.levelConnected = new int[connectArr.size()];
 			this.levelDisplay = new String[displayArr.size()];
-			this.levelConnected = new int[displayArr.size()];
-			this.levelPrereq = new ArrayList[displayArr.size()];
+			this.levelPrereq = new ArrayList[prereqArr.size()];
+			this.levelReqSize =  new int[reqSizeArr.size()];
+			
 			int count = 0;
-			while(posIterator.hasNext() && connectIterator.hasNext() && displayIterator.hasNext() && prereqIterator.hasNext()) {
+			while(posIterator.hasNext()) {
 				Iterator<Long> pos = posIterator.next().iterator();
 				levelPos.add(new Vector2(pos.next(), pos.next()));
-				levelDisplay[count] = displayIterator.next();
+				
 				levelConnected[count] = Math.toIntExact(connectIterator.next());
+				
+				levelDisplay[count] = displayIterator.next();
+				
 				levelPrereq[count] = new ArrayList<>();
 				Iterator<Long> prereq = prereqIterator.next().listIterator();
 				prereq.forEachRemaining(levelPrereq[count]::add);
+				
+				levelReqSize[count] = Math.toIntExact(reqSizeIterator.next());
+				
 				count++;
             }
 		}
