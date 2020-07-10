81ef259d217355aaf6986437f7e63e226a7f8fc4
MuteMini <63823931+MuteMini@users.noreply.github.com>
2020-07-09 22:06:38 -0400

    removed unneeded enums

--- sokoswitch/core/src/sokoswitch/game/level/Tiles.java
+++ sokoswitch/core/src/sokoswitch/game/level/Tiles.java
@@ -3,26 +3,15 @@ package sokoswitch.game.level;
 import java.util.HashMap;
 
 public enum Tiles {
-	
-	LEVELROAD(-4, 0, 1),
-	LOCKED_LEVELTILE(-3, 1, 0),
-	UNSOLVED_LEVELTILE(-2, 1, 1),
-	SOLVED_LEVELTILE(-1, 1, 2),
-	EMPTY(0, 0, 0),
-	SPACE(1, 1, 0),
-	PLAYER(2, 1, 3),
-	INVERSE_PLAYER(3, 1, 4),
-	BLOCK_OFF(4, 2, 2),
-	BLOCK_ON(5, 2, 2),
-	LOCKED_BLOCK_OFF(6, 2, 3),
-	LOCKED_BLOCK_ON(7, 2, 3);
+
+	EMPTY(0, false),
+	SPACE(1, true);
 	
 	public final static int SIZE = 256;
 	private static HashMap<Integer, Tiles> blockMap;
 	
 	private int id;
-	private int textureId;
-	private int texturePos;
+	private boolean moveOn;
 	
 	static {
 		blockMap = new HashMap<>();
@@ -39,17 +28,12 @@ public enum Tiles {
 		return id;
 	}
 	
-	public int getTextureId() {
-		return textureId;
+	public boolean getMoveOn() {
+		return moveOn;
 	}
 	
-	public int getTexturePos() {
-		return texturePos;
-	}
-	
-	private Tiles(int id, int textureId, int texturePos) {
+	private Tiles(int id, boolean moveOn) {
 		this.id = id;
-		this.textureId = textureId;
-		this.texturePos = texturePos;
+		this.moveOn = moveOn;
 	}
 }
